9754af12f38d7e898027806a8beb4373f1fe04b8
Rami Sbahi <sbahirami@gmail.com>
2023-09-06 22:33:16 -0500

    remove unnecessary stuff from node info

--- src/components/NodeInfo.tsx
+++ src/components/NodeInfo.tsx
@@ -1,11 +1,10 @@
-import { Node, useReactFlow } from "reactflow";
+import { Node } from "reactflow";
 import {
   Tabs,
   TabList,
   TabPanels,
   Tab,
   TabPanel,
-  Badge,
   Tag,
   TagLeftIcon,
   TagLabel,
@@ -13,29 +12,16 @@ import {
   ListItem,
   ListIcon,
   Heading,
-  Stat,
-  StatLabel,
-  StatNumber,
-  StatHelpText,
-  StatArrow,
-  StatGroup,
 } from "@chakra-ui/react";
 import { CheckIcon } from "@chakra-ui/icons";
 import { MdCheck, MdQuestionMark, MdClose, MdThumbUpOffAlt } from "react-icons/md";
-import { Column, Row } from "../utils/chakra";
+import { Column } from "../utils/chakra";
 import { BigButton } from "./utils/BigButton";
-import {
-  FluxNodeData,
-  FluxNodeType,
-  HistoryItem,
-  Settings,
-  CreateChatCompletionStreamResponseChoicesInner,
-  ReactFlowNodeTypes,
-} from "../utils/types";
+import { FluxNodeData, FluxNodeType, Settings } from "../utils/types";
 import { Prompt } from "./Prompt";
 import { ToTNodeData } from "./tree";
-import { getPlatformModifierKey, getPlatformModifierKeyText } from "../utils/platform";
-import { getFluxNodeTypeColor, getFluxNodeTypeDarkColor } from "../utils/color";
+import { getPlatformModifierKeyText } from "../utils/platform";
+import { getFluxNodeTypeDarkColor } from "../utils/color";
 
 function EvalListItem({ item }: { item: string }) {
   const lines = item.split("\n");
@@ -93,108 +79,107 @@ export function NodeInfo({
   return (
     <div className="node-info">
       <Tabs>
-      <TabList>
-        <Tab>Details</Tab>
-        <Tab>Conversation</Tab>
-      </TabList>
+        <TabList>
+          <Tab>Details</Tab>
+          <Tab>Conversation</Tab>
+        </TabList>
 
-      <TabPanels>
-        <TabPanel className="tab-panel-full-width">
-      {selectedNode?.data.isTerminal ? (
-        <Tag
-          size="lg"
-          variant="subtle"
-          colorScheme="purple"
-          style={{ marginRight: "0.5rem" }}
-        >
-          <TagLeftIcon boxSize="12px" as={MdThumbUpOffAlt} />
-          <TagLabel>Terminal</TagLabel>
-        </Tag>
-      ) : null}
-      {selectedNode?.data.isValid ? (
-        <Tag size="lg" variant="subtle" colorScheme="green">
-          <TagLeftIcon boxSize="12px" as={CheckIcon} />
-          <TagLabel>Valid</TagLabel>
-        </Tag>
-      ) : null}
-
-      <Heading as="h4" size="md">
-        Input
-      </Heading>
-      <p>{selectedNode?.data.input ?? ""}</p>
-      <Heading as="h4" size="md">
-        Score
-      </Heading>
-      <p>{selectedNode?.data.score ?? ""}</p>
-      {selectedNode?.data.output != "" && (
-        <>
-          <Heading as="h4" size="md">
-            Output
-          </Heading>
-          <p>{selectedNode?.data.output ?? ""}</p>
-        </>
-      )}
-      {selectedNode?.data?.steps && selectedNode?.data?.steps.length > 0 && (
-        <>
-          <Heading as="h4" size="md">
-            Steps
-          </Heading>
-          <List className="eval-list" spacing={3}>
-            {selectedNode?.data?.steps?.map((item, i) => {
-              return <ListItem key={i}>{item}</ListItem>;
-            })}
-          </List>
-        </>
-      )}
-      {selectedNode?.data?.evals && selectedNode?.data?.evals.length > 0 && (
-        <>
-          <Heading as="h4" size="md">
-            Evaluations
-          </Heading>
-          <List className="eval-list" spacing={3}>
-            {selectedNode?.data?.evals?.map((item, i) => {
-              return <EvalListItem key={i} item={item} />;
-            })}
-          </List>
-        </>
-      )}
-
-      </TabPanel>
-        <TabPanel className="tab-panel-full-width">
-          {lineage && lineage.length >= 1 ? (
-            <Prompt
-              settings={settings}
-              setSettings={setSettings}
-              isGPT4={isGPT4}
-              selectNode={selectNode}
-              newConnectedToSelectedNode={newConnectedToSelectedNode}
-              lineage={lineage}
-              onType={onPromptType}
-              submitPrompt={() => submitPrompt(false)}
-              apiKey={apiKey}
-            />
-          ) : (
-            <Column
-              expand
-              textAlign="center"
-              mainAxisAlignment={"center"}
-              crossAxisAlignment={"center"}
-            >
-              <BigButton
-                tooltip={`⇧${getPlatformModifierKeyText()}P`}
-                width="400px"
-                height="100px"
-                fontSize="xl"
-                onClick={() => onCreateNewConversation()}
-                color={getFluxNodeTypeDarkColor(FluxNodeType.GPT)}
+        <TabPanels>
+          <TabPanel className="tab-panel-full-width">
+            {selectedNode?.data.isTerminal ? (
+              <Tag
+                size="lg"
+                variant="subtle"
+                colorScheme="purple"
+                style={{ marginRight: "0.5rem" }}
               >
-                Create a new conversation tree
-              </BigButton>
-            </Column>
-          )}
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+                <TagLeftIcon boxSize="12px" as={MdThumbUpOffAlt} />
+                <TagLabel>Terminal</TagLabel>
+              </Tag>
+            ) : null}
+            {selectedNode?.data.isValid ? (
+              <Tag size="lg" variant="subtle" colorScheme="green">
+                <TagLeftIcon boxSize="12px" as={CheckIcon} />
+                <TagLabel>Valid</TagLabel>
+              </Tag>
+            ) : null}
+
+            <Heading as="h4" size="md">
+              Input
+            </Heading>
+            <p>{selectedNode?.data.input ?? ""}</p>
+            <Heading as="h4" size="md">
+              Score
+            </Heading>
+            <p>{selectedNode?.data.score ?? ""}</p>
+            {selectedNode?.data.output != "" && (
+              <>
+                <Heading as="h4" size="md">
+                  Output
+                </Heading>
+                <p>{selectedNode?.data.output ?? ""}</p>
+              </>
+            )}
+            {selectedNode?.data?.steps && selectedNode?.data?.steps.length > 0 && (
+              <>
+                <Heading as="h4" size="md">
+                  Steps
+                </Heading>
+                <List className="eval-list" spacing={3}>
+                  {selectedNode?.data?.steps?.map((item, i) => {
+                    return <ListItem key={i}>{item}</ListItem>;
+                  })}
+                </List>
+              </>
+            )}
+            {selectedNode?.data?.evals && selectedNode?.data?.evals.length > 0 && (
+              <>
+                <Heading as="h4" size="md">
+                  Evaluations
+                </Heading>
+                <List className="eval-list" spacing={3}>
+                  {selectedNode?.data?.evals?.map((item, i) => {
+                    return <EvalListItem key={i} item={item} />;
+                  })}
+                </List>
+              </>
+            )}
+          </TabPanel>
+          <TabPanel className="tab-panel-full-width">
+            {lineage && lineage.length >= 1 ? (
+              <Prompt
+                settings={settings}
+                setSettings={setSettings}
+                isGPT4={isGPT4}
+                selectNode={selectNode}
+                newConnectedToSelectedNode={newConnectedToSelectedNode}
+                lineage={lineage}
+                onType={onPromptType}
+                submitPrompt={() => submitPrompt(false)}
+                apiKey={apiKey}
+              />
+            ) : (
+              <Column
+                expand
+                textAlign="center"
+                mainAxisAlignment={"center"}
+                crossAxisAlignment={"center"}
+              >
+                <BigButton
+                  tooltip={`⇧${getPlatformModifierKeyText()}P`}
+                  width="400px"
+                  height="100px"
+                  fontSize="xl"
+                  onClick={() => onCreateNewConversation()}
+                  color={getFluxNodeTypeDarkColor(FluxNodeType.GPT)}
+                >
+                  Create a new conversation tree
+                </BigButton>
+              </Column>
+            )}
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
   );
 }
